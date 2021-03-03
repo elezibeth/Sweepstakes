14cfea8601b961db993d91e7043cbd17e139ce07
elezibeth <elezibethmorgan@my.unt.edu>
2021-03-03 13:58:48 -0800

    SweepstakesStackManager class preliminaries added.

---
+++ Sweepstakes/SweepstakesStackManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class SweepstakesStackManager : ISweepstakesManager
+    {
+        Stack<string> sweepStack;
+        public string name;
+
+        public SweepstakesStackManager(string name)
+        {
+            sweepStack = new Stack<string>();
+            this.name = name;
+            
+
+        }
+        //methods
+
+        //Push()
+
+        //Pop()
+        //FIFO
+
+
+    }
+}
