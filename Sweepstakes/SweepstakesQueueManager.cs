31850631244fd7589a2df757fd5c20e72b3393b2
elezibeth <elezibethmorgan@my.unt.edu>
2021-03-03 14:02:44 -0800

    Queue manager class added with preliminaries.

---
+++ Sweepstakes/SweepstakesQueueManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class SweepstakesQueueManager
+    {
+        Stack<string> sweepStack;
+        public string name;
+
+        public SweepstakesQueueManager(string name)
+        {
+            sweepStack = new Stack<string>();
+            this.name = name;
+            
+        }
+        //methods
+
+        //enqueue()add
+
+        //Dequeue() remove
+    }
+}
